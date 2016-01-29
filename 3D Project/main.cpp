dc670e666781f656dbbfdfbfd1b9ee33c4c28577
Timmie Pettersson <timmie.pettersson@gmail.com>
2016-01-29 13:53:31 +0100

    Initialized D3D

--- 3D Project/main.cpp
+++ 3D Project/main.cpp
@@ -1,16 +1,37 @@
 #include <windows.h>
 
+#include <d3d11.h>
+#include <d3dcompiler.h>
+#include <DirectXMath.h>
+
+#pragma comment (lib, "d3d11.lib")
+#pragma comment (lib, "d3dcompiler.lib")
+
 // will hold handle to access window
 HWND windowHandle = 0;
 
 UINT windowWidth = 800;
 UINT windowHeight = 600;
 
+const bool DEBUG = true;
+
+UINT msaaSamples = 4;
+
+D3D_FEATURE_LEVEL* featureLevel = nullptr;
+
+ID3D11Device* device = nullptr;
+ID3D11DeviceContext* deviceContext = nullptr;
+IDXGISwapChain* swapChain = nullptr;
+ID3D11RenderTargetView* backBufferRenderTargetView;
+
 // declare window procedure function
 LRESULT CALLBACK windowProc(HWND windowHandle, UINT message, WPARAM wParam, LPARAM lParam);
 
+void initializeD3D();
+
 int WINAPI WinMain(HINSTANCE instance, HINSTANCE prevInstance, PSTR cmdLine, int showCommand)
 {
+#pragma region window setup
 	// create and register window class
 	WNDCLASS windowClass;
 	windowClass.cbClsExtra = 0;
@@ -32,6 +53,9 @@ int WINAPI WinMain(HINSTANCE instance, HINSTANCE prevInstance, PSTR cmdLine, int
 	// show and update window
 	ShowWindow(windowHandle, SW_SHOW);
 	UpdateWindow(windowHandle);
+#pragma endregion
+
+	initializeD3D();
 
 	// main loop
 	MSG msg = { 0 };
@@ -70,4 +94,100 @@ LRESULT CALLBACK windowProc(HWND windowHandle, UINT message, WPARAM wParam, LPAR
 		return 0;
 	}
 	return DefWindowProc(windowHandle, message, wParam, lParam);
+}
+
+void initializeD3D()
+{
+	// create device, device context
+	D3D11CreateDevice(
+		NULL,
+		D3D_DRIVER_TYPE_HARDWARE,
+		NULL,
+		DEBUG == true ? D3D11_CREATE_DEVICE_DEBUG : NULL,
+		NULL,
+		0,
+		D3D11_SDK_VERSION,
+		&device,
+		featureLevel,
+		&deviceContext);
+
+	// check supported MSAA quality levels and store in msaaQualityLevels
+	UINT msaaQualityLevels;
+	device->CheckMultisampleQualityLevels(DXGI_FORMAT_R8G8B8A8_UNORM, msaaSamples, &msaaQualityLevels);
+
+	// describe swap chain
+	DXGI_SWAP_CHAIN_DESC swapChainDesc;
+	swapChainDesc.BufferDesc.Width						= windowWidth;
+	swapChainDesc.BufferDesc.Height						= windowHeight;
+	swapChainDesc.BufferDesc.RefreshRate.Numerator		= 60;
+	swapChainDesc.BufferDesc.RefreshRate.Denominator	= 1;
+	swapChainDesc.BufferDesc.Format						= DXGI_FORMAT_R8G8B8A8_UNORM;
+	swapChainDesc.BufferDesc.ScanlineOrdering			= DXGI_MODE_SCANLINE_ORDER_UNSPECIFIED;
+	swapChainDesc.BufferDesc.Scaling					= DXGI_MODE_SCALING_UNSPECIFIED;
+
+	swapChainDesc.SampleDesc.Count		= msaaSamples;
+	swapChainDesc.SampleDesc.Quality	= msaaQualityLevels > 0 ? msaaQualityLevels - 1 : 0;
+
+	swapChainDesc.BufferUsage	= DXGI_USAGE_RENDER_TARGET_OUTPUT;
+	swapChainDesc.BufferCount	= 1;
+	swapChainDesc.OutputWindow	= windowHandle;
+	swapChainDesc.Windowed		= true;
+	swapChainDesc.SwapEffect	= DXGI_SWAP_EFFECT_DISCARD;
+	swapChainDesc.Flags			= NULL;
+	
+	// create swap chain
+	IDXGIDevice* DXGIDevice = nullptr;
+	device->QueryInterface(__uuidof(IDXGIDevice), (void**) &DXGIDevice);
+	IDXGIDevice* DXGIAdapter = nullptr;
+	DXGIDevice->GetParent(__uuidof(IDXGIAdapter), (void**) &DXGIAdapter);
+	IDXGIFactory* DXGIFactory = nullptr;
+	DXGIAdapter->GetParent(__uuidof(IDXGIFactory), (void**) &DXGIFactory);
+
+	DXGIFactory->CreateSwapChain(device, &swapChainDesc, &swapChain);
+
+	DXGIDevice->Release();
+	DXGIAdapter->Release();
+	DXGIFactory->Release();
+
+	// create render target view for back buffer
+	ID3D11Texture2D* backBuffer;
+	swapChain->GetBuffer(0, __uuidof(ID3D11Texture2D), (void**) &backBuffer);
+	device->CreateRenderTargetView(backBuffer, 0, &backBufferRenderTargetView);
+	backBuffer->Release();
+
+	// create depth buffer and view
+	D3D11_TEXTURE2D_DESC depthBufferDesc;
+	depthBufferDesc.Width				= windowWidth;
+	depthBufferDesc.Height				= windowHeight;
+	depthBufferDesc.MipLevels			= 1;
+	depthBufferDesc.ArraySize			= 1;
+	depthBufferDesc.Usage				= D3D11_USAGE_DEFAULT;
+	depthBufferDesc.CPUAccessFlags		= 0;
+	depthBufferDesc.MiscFlags			= 0;
+	depthBufferDesc.BindFlags			= D3D11_BIND_DEPTH_STENCIL;
+	depthBufferDesc.Format				= DXGI_FORMAT_D24_UNORM_S8_UINT;
+	depthBufferDesc.SampleDesc.Count	= msaaSamples;
+	depthBufferDesc.SampleDesc.Quality	= msaaQualityLevels > 0 ? msaaQualityLevels - 1 : 0;
+
+	ID3D11Texture2D* depthBuffer;
+
+	device->CreateTexture2D(&depthBufferDesc, 0, &depthBuffer);
+
+	ID3D11DepthStencilView* depthView;
+
+	device->CreateDepthStencilView(depthBuffer, 0, &depthView);
+
+	// bind to output merger
+	deviceContext->OMGetRenderTargets(1, &backBufferRenderTargetView, &depthView);
+
+	// create viewport
+	D3D11_VIEWPORT viewPort;
+	viewPort.Width = windowWidth;
+	viewPort.Height = windowHeight;
+	viewPort.TopLeftX = 0;
+	viewPort.TopLeftY = 0;
+	viewPort.MaxDepth = 1.0f;
+	viewPort.MinDepth = 0.0f;
+
+	deviceContext->RSSetViewports(1, &viewPort);
 }
